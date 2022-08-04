20ff0fe964e1ba2e2d937912f7f91015e00e5c72
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-08-04 10:57:41 +0100

    Lab complete usong reactdom v5

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { directors } from "../data";
+import {nanoid} from "nanoid"
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map(director=>
+        <div key={nanoid(5)}>
+          <h1>{director.name}</h1>
+          <ul>{director.movies.map(movie => 
+            <li key={nanoid(5)}>{movie}</li>
+            )}
+          </ul>
+        </div>
+        )}
+    </div>);
 }
 
 export default Directors;
